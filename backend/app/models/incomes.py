5f7658836764a3f2c623c3688cb9041c906b15b9
Eduarda Abreu <eduarda.g.abreu.a@gmail.com>
2026-04-23 15:38:20 -0300

    SM-11 cria tabela incomes

--- backend/app/models/incomes.py
+++ backend/app/models/incomes.py
@@ -1,5 +1,5 @@
 import uuid
-from sqlalchemy import Column, String, Numeric, DateTime, Enum, func
+from sqlalchemy import Boolean, Column, Date, String, Numeric, DateTime, Enum, func
 from sqlalchemy.dialects.postgresql import UUID
 from app.core.database import Base
 import enum
@@ -8,10 +8,9 @@ class incomes(Base):
     __tablename__ = 'incomes'
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    userid = Column(UUID(as_uuid = True), forgein_key = true, nullable = false)
-    name = Column(String(100), nullable=False)
-    email = Column(String(255), unique=True, index=True, nullable=False)
-    pwd_hash = Column(String, nullable=False)
-    financial_profile = Column(Enum(FinancialProfile), default=FinancialProfile.BEGINNER)
-    monthly_income = Column(Numeric(12, 2), nullable=True)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+    value = Column(Numeric(12, 2), nullable=False)
+    description = Column(String(255))
+    recorrente = Column(Boolean, default=False)
+    date = Column( Date, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
